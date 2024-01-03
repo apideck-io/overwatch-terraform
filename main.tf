fb491262d545b4a94f77e3270dfc92d8a0664554
Geert Wille <geert@apideck.com>
2024-01-03 10:16:44 +0100

    Upgrade retool

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "retool" {
   private_subnet_ids = [for subnet in module.platform_network.main_private_subnets : subnet.id]
   public_subnet_ids  = [for subnet in module.platform_network.main_public_subnets : subnet.id]
   # ssh_key_pair = "<your-key-pair>"
-  ecs_retool_image = "tryretool/backend:3.10.1"
+  ecs_retool_image = "tryretool/backend:3.24.6"
   domain_name      = local.domain_name
 
   retool_license_key    = data.aws_ssm_parameter.retool_license_key.arn
